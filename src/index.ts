454ef749baca3d9ae624422d11ae29636ce8f011
linyimin <ahl5esoft@gmail.com>
2023-04-20 08:16:22 +0800

    docs: v1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from './now-time';
-export * from './now-time-base';
--- src/now-time.ts
+++ src/now-time.ts
@@ -1,13 +1,8 @@
+import { NowTimeBase } from 'lite-ts-time';
 import moment from 'moment';
-
-import { NowTimeBase } from './now-time-base';
 
 export class MomentNowTime extends NowTimeBase {
     public async unix() {
         return moment().unix();
-    }
-
-    public async unixNano() {
-        return moment().valueOf() * 1_000_000;
     }
 }
